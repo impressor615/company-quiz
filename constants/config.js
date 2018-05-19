03851523b62a3e425fb75c5ce003eebf9c17bbdc
seonghyeon <hyeon@apartmentary.com>
2018-05-19 21:48:10 +0900

    deploy script inital 추가

--- constants/config.js
+++
@@ -1,7 +0,0 @@
-export default {
-  LOGIN_TOKEN: 'todaycafes-admin-login',
-  GRAPHQL_SERVER: {
-    URI: 'http://localhost:4000',
-  },
-  API: 'https://localhost:3000',
-};
--- constants/index.js
+++ constants/index.js
@@ -1,7 +1,5 @@
 import style from './style';
-import config from './config';
 import database from './database';
 
-export const CONFIG = config;
 export const STYLE = style;
 export const DATABASE = database;
